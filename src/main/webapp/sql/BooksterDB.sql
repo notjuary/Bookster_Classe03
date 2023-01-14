9fc256e1ce0334fa078dc885483ea48b9748de1b
notjuary <barlottifrancesco@gmail.com>
2023-01-14 16:26:14 +0100

    Aggiunta del database nella cartella sql

---
+++ src/main/webapp/sql/BooksterDB.sql
@@ -0,0 +1,47 @@
+drop database if exists Bookster;
+create database Bookster;
+use Bookster;
+
+CREATE TABLE Lettore(
+username varchar(20) primary key,
+psw varchar(100) not null,
+nome varchar(50) not null, 
+cognome varchar(50) not null,
+email varchar(100) not null,
+ddn date not null,
+telefono varchar(11),
+genere varchar(20),
+punteggio int not null
+);
+
+CREATE TABLE Classifica(
+id int(10) primary key,
+partecipanti int(255) not null,
+periodo date not null
+);
+
+CREATE TABLE Libro(
+isbn int(10) primary key,
+titolo varchar(100) not null,
+autore date not null,
+pathcopertina varchar(255) not null default("none.jpg"),
+pagine int  not null
+);
+
+CREATE TABLE Libreria(
+nome int(10) not null,
+username varchar(20) not null,
+libri int not null,
+foreign key(nome) references Libro(isbn),
+foreign key(username) references Lettore(username)
+);
+
+CREATE TABLE Recensione(
+id int(10) primary key,
+stelle decimal(2,1) not null,
+testo varchar(255) not null
+);
+
+INSERT INTO Lettore VALUES
+("Goor1979",SHA1("tie1IJiengei"),"Demetrio","De Luca","demetriodeluca@teleworm.us",'2001-02-24',"3456003403",null,0),
+("Howl1944",SHA1("pei7Hoh0f"),"Ninfa", "Lo Duca","ninfaloduca@armyspy.com",'1987-10-05',"3456003403",null,50);
