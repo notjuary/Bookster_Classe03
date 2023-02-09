28b462529646b2722dca16b1eef8acd03ed6b251
grauso-t <grauso.teo@gmail.com>
2023-02-09 22:36:37 +0100

    Javadoc

--- src/main/java/Controller/ScoreServlet.java
+++ src/main/java/Controller/ScoreServlet.java
@@ -13,6 +13,16 @@ import java.io.IOException;
 @WebServlet( value = "/ScoreServlet")
 public class ScoreServlet extends HttpServlet {
 
+    /**
+     * Il metodo gestisce una richiesta doGet. Il metodo recupera i valori "pages" e "seconds" dalla richiesta HTTP.
+     * Se il tempo supera il numero di pagine moltiplicato per 10, viene effettuato un upgrade del punteggio del
+     * lettore presente nella sessione.
+     * @param request l'oggetto HttpServletRequest che contiene la richiesta che il client ha fatto alla servlet
+     * @param response l'oggetto HttpServletResponse che contiene la risposta che la servlet invia al client
+     * @throws IOException se viene rilevato un errore d'I/O nella gestione della richiesta
+     * @see Lettore
+     * @see LettoreDAO
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int pages = Integer.parseInt(request.getParameter("pages"));
--- src/main/java/Model/Libreria.java
+++ src/main/java/Model/Libreria.java
@@ -7,34 +7,62 @@ public class Libreria {
     private int numeroPagineTotale;
 
 
+    /**
+     * Il metodo restituisce il titolo
+     * @return titolo
+     */
     public String getTitolo() {
         return titolo;
     }
 
+    /**
+     * Il metodo setta il titolo
+     */
     public void setTitolo(String titolo) {
         this.titolo = titolo;
     }
 
+    /**
+     * Il metodo restituisce il numero di libri
+     * @return numero libri
+     */
     public int getNumeroLibri() {
         return numeroLibri;
     }
 
+    /**
+     * Il metodo setta il numero di libri
+     */
     public void setNumeroLibri(int numeroLibri) {
         this.numeroLibri = numeroLibri;
     }
 
+    /**
+     * Il metodo restituisce il numero di pagine lette
+     * @return numero pagine lette
+     */
     public int getNumeroPagineLette() {
         return numeroPagineLette;
     }
 
+    /**
+     * Il metodo setta il numero di pagine lette
+     */
     public void setNumeroPagineLette(int numeroPagineLette) {
         this.numeroPagineLette = numeroPagineLette;
     }
 
+    /**
+     * Il metodo restituisce il numero di pagine totale
+     * @return numero pagine totale
+     */
     public int getNumeroPagineTotale() {
         return numeroPagineTotale;
     }
 
+    /**
+     * Il metodo setta il numero delle pagine
+     */
     public void setNumeroPagineTotale(int numeroPagineTotale) {
         this.numeroPagineTotale = numeroPagineTotale;
     }
